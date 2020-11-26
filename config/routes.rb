53439bb2cbb630e28e2c50468fa1c8493f54889f
Renata Bresolin <renatagabriellib@gmail.com>
2020-11-26 18:42:18 -0300

    skipe almost done

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :products, shallow: true do
     resources :rentals
+    resources :charges
   end
   patch '/rentals/:rental', to: 'pages#return_product', as: :return_product
   get '/rentals', to: 'pages#rentals', as: :user_rentals
