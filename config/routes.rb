a730f4a8da04e2b70839a0d5fe29e9349a2ec8aa
Renata Bresolin <renatagabriellib@gmail.com>
2020-11-27 14:52:53 -0300

    product solved

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   root to: 'products#index'
   devise_for :users
   resources :products, shallow: true do
-    resources :rentals
+    resources :rentals, except: :index
     resources :charges
     get 'return', to: 'rentals#return'
   end
+
+  resources :rentals, only: :index
+
   patch '/rentals/:rental', to: 'pages#return_product', as: :return_product
   get '/rentals', to: 'pages#rentals', as: :user_rentals
   get '/search', to: 'products#search', as: :search_product
