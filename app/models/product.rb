0585e408f6365ca9ad3b3f9bc777817576ac5ab6
Renata Bresolin <renatagabriellib@gmail.com>
2021-01-11 16:38:41 -0300

    including metatags into webapp

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,11 +2,4 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :rentals
   has_one_attached :photo
-
-# include PgSearch::Model
-#   pg_search_scope :search_by_description,
-#     against: [ :description, :category ],
-#     using: {
-#       tsearch: { prefix: true }
-#     }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :photo
   has_many :products
   has_many :rentals
